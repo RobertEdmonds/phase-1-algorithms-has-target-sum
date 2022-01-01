b09b94cbef37255e6b2cb4b7adea5cc7121f77da
Robert Edmonds <bobby.edmonds89@gmail.com>
2021-12-31 17:05:09 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i=0;i<array.length;i++){
+    for(let j=i+1;j<array.length;j++){
+      if((array[i]+array[j])===target)
+        return true
+    }
+  }return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  0(n^2)
 */
 
 /* 
   Add your pseudocode here
+  initializing through the array
+  adding the numbers to each other two at a time
+  checking to see if it equals to the target
 */
 
 /*
   Add written explanation of your solution here
+  loop through the array adding one to another and checking to see if it is equal to the target
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +40,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 9, 17, 32], 12));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([2, 5, 12, 11], 14));
 }
 
 module.exports = hasTargetSum;
